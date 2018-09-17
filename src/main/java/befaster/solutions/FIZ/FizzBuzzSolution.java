26f951e477bf094d4411fe360b370903b5050d3d
sbx_user1060 <sbx_user1060@ip-10-128-135-255.eu-west-2.compute.internal>
2018-09-17 23:40:38 +0000

    Mon Sep 17 23:40:38 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -1,21 +1,16 @@
 package befaster.solutions.FIZ;
 
-import befaster.runner.SolutionNotImplementedException;
-
 public class FizzBuzzSolution {
 
     public String fizzBuzz(Integer number) {
-    	 StringBuilder sb = new StringBuilder();
-         if(number % 3 == 0) sb.append("fizz");
-         if(number % 5 == 0) sb.append(" buzz");
+   	    StringBuilder sb = new StringBuilder();
+        if(number % 3 == 0) sb.append("fizz");
+        if(number % 5 == 0) sb.append(" buzz");
 
-//         if(Integer.toString(number).indexOf("3") != -1) sb.append("fizz");
-//         if(Integer.toString(number).indexOf("5") != -1) sb.append("buzz");
-
-         	if (sb.length() == 0) 
-         	  return String.valueOf(number);
-         	else 
-        	 return sb.toString();
-         }
+     	if (sb.length() == 0) 
+     	  return String.valueOf(number);
+     	else 
+    	 return sb.toString().trim();
+    }
 
 }
