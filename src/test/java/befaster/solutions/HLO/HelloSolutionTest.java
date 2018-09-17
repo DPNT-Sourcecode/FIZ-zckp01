86449f8f97b90dd9519e34ce18d9fa6fe78dc359
sbx_user1060 <sbx_user1060@ip-10-128-135-255.eu-west-2.compute.internal>
2018-09-17 23:18:09 +0000

    Mon Sep 17 23:18:09 UTC 2018

--- src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
+++ src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
@@ -17,6 +17,6 @@ public class HelloSolutionTest {
 
     @Test
     public void compute_sum() {
-        assertThat(hello.hello("Amit"), equalTo("\"hello to the world \" + friendName"));
+        assertThat(hello.hello("Amit"), equalTo("hello to the world Amit"));
     }
 }
