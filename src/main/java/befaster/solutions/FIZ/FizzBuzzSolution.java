8ee16000fdb979a11acb58c6f47cb16e172f6623
sbx_user1060 <sbx_user1060@ip-10-128-135-255.eu-west-2.compute.internal>
2018-09-18 00:20:03 +0000

    Tue Sep 18 00:20:03 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -3,10 +3,18 @@ package befaster.solutions.FIZ;
 public class FizzBuzzSolution {
 
     public String fizzBuzz(Integer number) {
+    	
+    	
+    	  - A number is "fizz deluxe" if it is divisible by 3 AND it has a 3 in it
+    	  - A number is "buzz deluxe" if it is divisible by 5 AND it has a 5 in it
+    	  - We should still say "fake deluxe" if the "deluxe" number is odd
+    	  - A number can be "fizz", "buzz" and "deluxe" at the same time. If this happens then write "fizz buzz deluxe"
+    	  - All the previous rules for "fizz" and "buzz" are still valid    	
+    	
     	String num = Integer.toString(number);
    	    StringBuilder sb = new StringBuilder();
-        if(number % 3 == 0 || num.indexOf("3") != -1) sb.append("fizz");
-        if(number % 5 == 0 || num.indexOf("5") != -1) sb.append(" buzz");
+        if(number % 3 == 0 || num.indexOf("3") != -1) sb.append("fizz deluxe");
+        if(number % 5 == 0 || num.indexOf("5") != -1) sb.append(" buzz deluxe");
         
         if (number > 10 && num.matches("^([0-9])\\1*")) {
 	        if (number%2 != 0) 
