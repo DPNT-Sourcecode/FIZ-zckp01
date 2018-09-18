3e9b3c49c501ec5bcb14e6999b2b1bf6e50ef216
sbx_user1060 <sbx_user1060@ip-10-128-135-255.eu-west-2.compute.internal>
2018-09-18 00:10:44 +0000

    Tue Sep 18 00:10:44 UTC 2018

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -8,10 +8,12 @@ public class FizzBuzzSolution {
         if(number % 3 == 0 || num.indexOf("3") != -1) sb.append("fizz");
         if(number % 5 == 0 || num.indexOf("5") != -1) sb.append(" buzz");
         
-        if (number % 2 != 0) 
-        	sb.append(" fake deluxe");
-        else	
-        	if(number > 10 && num.matches("^([0-9])\\1*")) sb.append(" deluxe");
+        if (number > 10 && num.matches("^([0-9])\\1*")) {
+	        if (number % 2 != 0) 
+	        	sb.append(" fake deluxe");
+	        else	
+	        	sb.append(" deluxe");
+        }    
         
      	if (sb.length() == 0) 
      	  return String.valueOf(number);
